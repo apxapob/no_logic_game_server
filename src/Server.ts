3d1e8e3be18248fc19de216170ac7d1dde77a491
Александр Архаров <aarkharov@mastertigra.com>
2021-03-27 22:27:05 +0300

    getRooms and getPlayers

--- src/Server.ts
+++ src/Server.ts
@@ -3,34 +3,27 @@ import { Lobby } from "./Lobby"
 import { Player } from "./Player"
 import { Room } from "./Room"
 
+type playersObj = {
+  [key: string]: Player;
+};
+type roomsObj = {
+  [key: string]: Room;
+};
+
 export class Server {
   
   private wsServer:WebSocket.Server
 
-  public players:Map<string, Player> = new Map<string, Player>()
+  public players:playersObj = {}
   public lobby = new Lobby()
-  public rooms:Map<string, Room> = new Map<string, Room>()
+  public rooms:roomsObj = {}
 
   constructor(){
     this.wsServer = new WebSocket.Server({ port: 8080 })
 
     this.wsServer.on('connection', (ws, req) => {
-
       const pl = this.authPlayer(ws, req.url || '')
 
-      ws.on('message', message => {
-        console.log('@@', ws.readyState)
-        console.log(`Received message => ${message}`)
-      })
-      ws.on('error', e => {
-        console.log('socket error', e)
-      })
-      ws.on('close', () => {
-        console.log('connection closed')
-        console.log('@@@', ws.readyState)
-      })
-
-      
       if(!pl){
         console.log('someone connected', req.url)
         ws.send(JSON.stringify({
@@ -38,9 +31,46 @@ export class Server {
           data: `Wrong login params`
         }))
         ws.close()
+      } else {
+        ws.on('message', message => {
+          console.log(`Received message => ${message}`)
+          
+          const msg = JSON.parse(message.toString())
+          this.onGetMessage(pl, msg)
+        })
+        ws.on('error', e => {
+          console.log('socket error', e)
+        })
+        ws.on('close', () => {
+          console.log('connection closed')
+          console.log('@@@', ws.readyState)
+        })
       }
       
     })
+  }
+
+  onGetMessage(pl:Player, msg:any){
+    switch(msg.method){
+      case "getRooms":
+        pl.ws.send(JSON.stringify({
+          method: 'onGetRooms',
+          data: Object.values(this.rooms).map(r => ({
+            id: r.roomId,
+            players: r.playersId
+          }))
+        }))
+        return
+      case "getPlayers":
+        pl.ws.send(JSON.stringify({
+          method: 'onGetPlayers',
+          data: Object.values(this.players).map( pl => ({
+            id: pl.playerId,
+            name: pl.playerName
+          }) )
+        }))
+        return
+    }
   }
 
   authPlayer(ws:WebSocket, url:string):Player | null {
@@ -50,7 +80,7 @@ export class Server {
     const token = loginParams.get("token")
     if(!playerName || !token){ return null }
 
-    let pl = this.players.get(playerName+"_"+token)
+    let pl:Player = this.players[token]
     
     if(pl){
       ws.send(JSON.stringify({
@@ -60,7 +90,7 @@ export class Server {
       pl.ws = ws
     } else {
       pl = new Player(playerName, token, ws)
-      this.players.set(playerName+"_"+token, pl)
+      this.players[token] = pl
       ws.send(JSON.stringify({
         method: 'message',
         data: `Hi, ${playerName}!`

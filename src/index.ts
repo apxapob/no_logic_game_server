5596358a8061e39b65ef011449bee0d77f62333e
Александр Архаров <aarkharov@mastertigra.com>
2021-04-02 21:11:10 +0300

    catch all exceptions

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Server } from "./Server"
 
 console.clear()
 
+process.on('uncaughtException', err => {
+  console.error('Caught exception: ' + err);
+});
+
 let serverInstance = new Server()
 console.log("server started")
 
